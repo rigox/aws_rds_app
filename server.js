4fb0f0d185fbb58e2c2e3d9d44d3f263643067e9
rigox <rigoc1994@gmail.com>
2019-10-20 20:54:07 -0400

    api is built badly but this is just to test out a deployment

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const app = express()
 const dotenv  = require("dotenv")
 const PORT =  process.env.PORT  || 5000
 const mysql =  require("mysql")
+const cors =  require("cors")
 
 //load enviromental variables
 
 dotenv.config({path:'./config/config.env'})
+//apply middleware
+app.use(express.json(),express.urlencoded({extended:true}))
+app.use(cors())
 
 var connection = mysql.createConnection({
     host:process.env.host,
@@ -19,13 +23,21 @@ var connection = mysql.createConnection({
 
 connection.connect()
 
-connection.query('select * from users',function(err,results,fields  ){
-  if(err){throw err}
-   console.log('results ' ,  results)
+app.post('/api/user/register',(req,res)=>{
+
+        const  {name , email , password} = req.body
+        connection.query('insert into users(name,email,password) values(?,?,?)' ,[name,email, password], function(err,results,fiels){
+            if(err){throw err}
+            console.log("records inserted" , results)
+        });
+
+
+
 });
 
-//apply middleware
-app.use(express.json(),express.urlencoded({extended:true}))
+
+
+
 
 
 app.listen(PORT, ()=>{
